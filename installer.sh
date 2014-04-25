01e561f02ebe4b8dd7076b18c7285d62d4bf19f9
Dima Aleksandrov <elixmoon@gmail.com>
2014-04-25 08:11:40 +0200

    doesn't separate installer scripts

--- installer.sh
+++ installer.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 mkdir -p temp
 cd temp
-curl https://raw.github.com/travelping/tetrapak_deps/master/cloner.sh | sh
-curl https://raw.github.com/travelping/tetrapak_deps/master/build_install.sh | sh
-cd ..
+
+git clone https://github.com/travelping/tetrapak.git
+git clone https://github.com/travelping/sh.git
+git clone https://github.com/travelping/erlsemver.git
+git clone https://github.com/liveforeverx/redbug.git
+git clone https://github.com/travelping/erlgit.git
+git clone https://github.com/travelping/tetrapak_deps.git
+
+cd tetrapak
+make install
+curl https://raw.github.com/travelping/tetrapak_deps/master/linker.escript > linker.escript
+escript linker.escript
+cd ../sh
+tetrapak install -local
+cd ../erlsemver
+tetrapak install -local
+cd ../redbug
+tetrapak install -local
+cd ../erlgit
+tetrapak install -local
+cd ../tetrapak_deps
+tetrapak install -local
+
+cd ../..
 rm -rf temp
